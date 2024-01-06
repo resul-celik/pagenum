86b8808c5b8fe43f328b330e1eab46666da2f08c
resulc <>
2024-01-06 10:35:29 +0300

    new features

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Figma Pagenum Plugin
-Pagenum lets you create page numbers within frames. You can add numbers easily.
+
+Pagenum makes it simple to create page numbers within frames.
 <img src="images/pagenum.gif" width="400" />
 
-How to use:
+## How it works
 
-- Insert the pattern into the frame(s)
-- Select all those frames
-- Click 'Numerate' button
+### 1. Add the pattern into the frames
+
+You should start by adding the pattern to the frames. When you open the plugin, you can see the pattern. The pattern's default value is $p, but you can change it. Make sure it's unique and doesn't include any terms from the text that you don't want to change.
+
+### 2. Select all those frames that have your pattern
+
+When you've decided on your pattern and inserted it into the frames you want to order, click the 'Paginate' button. The frames will then be paginated by frame index by Pagenum.
+
+**It's important: You can change the frame order in Figma's layers panel if you change the index.**
--- code.ts
+++ code.ts
@@ -1,106 +1,127 @@
-figma.showUI(__html__, { visible: true, width: 370, height: 650 });
+figma.showUI(__html__, { visible: true, width: 400, height: 700 });
 
-figma.on("selectionchange", (event) => {
-
-  var selectedFrames = '<div class="nothing">There are no frames selected.</div>'
-  
-  for (const selections of figma.currentPage.selection) {
-
-    if (selections.type == 'FRAME') {
-
-      selectedFrames = 'Frames are selected.';
-
-    } 
-
-  }
-  
-  figma.ui.postMessage(selectedFrames, { origin: "*" });
-
+figma.on("selectionchange", () => {
+    var selectedFrames = '';
+    for (const selections of figma.currentPage.selection) {
+        if (selections.type == 'FRAME' || selections.type == 'SECTION') {
+            selectedFrames = 'Frame(s) selected.';
+        }
+    }
+    figma.ui.postMessage(selectedFrames, { origin: "*" });
 });
 
 figma.ui.onmessage = msg => {
-  
-  if (msg.type === 'page-numbers') {
-
-    pageNums(msg)
-
-  }
-
+    if (msg.type === 'page-numbers') {
+        pageNums(msg);
+    }
 };
 
-async function pageNums(msg) {
+/* Functions (Start) */
+// To roman
 
-  var startNumber = 1;
-  var detectedPatterns = 0;
-  var pattern = '$p';
-  var frames = false;
-
-  if (msg.startNumber) {
-
-    startNumber = msg.startNumber
-
-  }
-
-  if (msg.pattern) {
-
-    pattern = msg.pattern
-
-  }
-
-  for (const selection of figma.currentPage.selection) {
-
-    if (selection.type == 'FRAME') {
-
-      var textObjects = selection.findAll(selection => selection.type === "TEXT").filter(selection => selection.characters === pattern)
-
-      textObjects.forEach(function (text) {
-
-        var newText = startNumber.toString()
-        
-        detectedPatterns++
-
-        figma.loadFontAsync(text.fontName).then(() => { text.characters = newText })
-
-      })
-
-      if (textObjects.length != 0) {
-
-        if (msg.direction) {
-
-          startNumber++
-
-        } else {
-
-          startNumber--
-
+const toRoman = (num:any) => {
+    let result = '';
+    const romanNumerals = [
+        { letter: 'M', value: 1000 },
+        { letter: 'CM', value: 900 },
+        { letter: 'D', value: 500 },
+        { letter: 'CD', value: 400 },
+        { letter: 'C', value: 100 },
+        { letter: 'XC', value: 90 },
+        { letter: 'L', value: 50 },
+        { letter: 'XL', value: 40 },
+        { letter: 'X', value: 10 },
+        { letter: 'IX', value: 9 },
+        { letter: 'V', value: 5 },
+        { letter: 'IV', value: 4 },
+        { letter: 'I', value: 1 },
+    ];
+    for (let i = 0; i < romanNumerals.length; i++) {
+        const numeral = romanNumerals[i];
+        const value = numeral.value;
+        while (num >= value) {
+            result += numeral.letter;
+            num -= value;
         }
+    }
+    return result;
+}
 
-      }
+/* Functions (End) */
+const pageNums = async (msg:any) => {
+    
+    var startNumber = 1;
+    var detectedPatterns = 0;
+    var pattern = '$p';
+    var frames = false;
+    var selectionIndex = [];
 
-      frames = true
-
+    if (msg.startNumber) {
+        startNumber = msg.startNumber;
     }
 
-  }
-
-  // Feedbacks
-
-  if (frames) {
-
-    if (detectedPatterns == 0) {
-
-      figma.notify(`There are no pattern detected.`);
-
-    } else {
-
-      figma.notify(`Pages successfully numbered.`);
-
+    if (msg.pattern) {
+        pattern = msg.pattern;
     }
 
-  } else {
+    for (const selections of figma.currentPage.selection) {
+        if (selections.type == 'FRAME' || selections.type == 'SECTION') {
+            selectionIndex.push({
+                index: figma.currentPage.children.indexOf(selections),
+                id: selections.id
+            });
 
-    figma.notify(`There are no frames selected. `);
+            selectionIndex.sort(function(a, b) {
+                return b.index - a.index;
+            })
+        }
+    }
+    for (const frameIndex of selectionIndex) {
+        var frameNode:any = figma.getNodeById(frameIndex.id);
+        var textObjects = frameNode?.findAll((frameNode: { type: string; }) => frameNode.type === "TEXT").filter((frameNode: { characters: string; }) => frameNode.characters === pattern);
+        
+        textObjects.forEach(function (text:any) {
+            var newText = startNumber.toString()
 
-  }
+            if (!msg.style) {
+                var newText = toRoman(startNumber);
+            }
 
+            detectedPatterns++
+
+            figma.loadFontAsync(text.fontName).then(() => { text.characters = newText })
+
+            if (textObjects.length != 0) {
+    
+            if (msg.direction) {
+    
+                startNumber++
+    
+            } else {
+    
+                startNumber--
+    
+            }
+    
+            }
+    
+            
+        }) 
+
+        frames = true
+    }
+        
+    // Feedbacks
+    if (frames) {
+        if (detectedPatterns == 0) {
+            figma.notify(`There is no pattern to be matched.`);
+        }
+        else {
+            figma.notify(`Pages were successfully paginated.`);
+        }
+    }
+    else {
+        figma.notify(`Please select at least one frame first. `);
+    }
+    
 }
